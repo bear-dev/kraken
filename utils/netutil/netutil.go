80c533f7a7fbc39e51c1b51b05f51cc1799627c0
Alexander Korobkov <akorobkov@nvidia.com>
2025-05-05 13:49:26 -0600

    feat: Hostnetwork support

--- utils/netutil/netutil.go
+++ utils/netutil/netutil.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,7 +22,7 @@ import (
 
 // _supportedInterfaces is an ordered list of ip interfaces from which
 // host ip is determined.
-var _supportedInterfaces = []string{"eth0", "ib0", "eth2"}
+var _supportedInterfaces = []string{"eth0", "ib0", "eth2", "brens8"}
 
 func min(a, b time.Duration) time.Duration {
 	if a < b {
